0731f8ba5b3a9b935a8f5e8202ae4041e055645e
Chris Dennett <chris.dennett@valtech.co.uk>
2017-12-06 09:19:24 +0000

    updated date picker and enhanced journey

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,3 +27,147 @@ $(document).on('ready',function()
     window.location.href = href;
   });
 });
+
+
+
+
+$("input[type='checkbox'][class='mon-fil']").on("change",function(){
+  if($(this).is(':checked')){
+    $("#monday-filter").show();
+  }else{
+    $("#monday-filter").hide();
+  }
+});
+
+$("input[type='checkbox'][class='tue-fil']").on("change",function(){
+  if($(this).is(':checked')){
+    $("#tuesday-filter").show();
+  }else{
+    $("#tuesday-filter").hide();
+  }
+});
+
+$("input[type='checkbox'][class='wed-fil']").on("change",function(){
+  if($(this).is(':checked')){
+    $("#wednesday-filter").show();
+  }else{
+    $("#wednesday-filter").hide();
+  }
+});
+
+$("input[type='checkbox'][class='thu-fil']").on("change",function(){
+  if($(this).is(':checked')){
+    $("#thursday-filter").show();
+  }else{
+    $("#thursday-filter").hide();
+  }
+});
+
+$("input[type='checkbox'][class='fri-fil']").on("change",function(){
+  if($(this).is(':checked')){
+    $("#friday-filter").show();
+  }else{
+    $("#friday-filter").hide();
+  }
+});
+
+$("input[type='checkbox'][class='sat-fil']").on("change",function(){
+  if($(this).is(':checked')){
+    $("#saturday-filter").show();
+  }else{
+    $("#saturday-filter").hide();
+  }
+});
+
+
+$("input[type='checkbox'][class='time-slot1']").on("change",function(){
+  if($(this).is(':checked')){
+    $("#time1-filter").show();
+  }else{
+    $("#time1-filter").hide();
+  }
+});
+
+$("input[type='checkbox'][class='time-slot2']").on("change",function(){
+  if($(this).is(':checked')){
+    $("#time2-filter").show();
+  }else{
+    $("#time2-filter").hide();
+  }
+});
+
+$("input[type='checkbox'][class='time-slot3']").on("change",function(){
+  if($(this).is(':checked')){
+    $("#time3-filter").show();
+  }else{
+    $("#time3-filter").hide();
+  }
+});
+
+$("input[type='checkbox'][class='access-floor']").on("change",function(){
+  if($(this).is(':checked')){
+    $("#special-access").show();
+  }else{
+    $("#special-access").hide();
+  }
+});
+$("input[type='checkbox'][class='interpreter']").on("change",function(){
+  if($(this).is(':checked')){
+    $("#special-interpreter").show();
+  }else{
+    $("#special-interpreter").hide();
+  }
+});
+
+$("input[type='checkbox'][class='same-gender']").on("change",function(){
+  if($(this).is(':checked')){
+    $("#special-gender").show();
+  }else{
+    $("#special-gender").hide();
+  }
+});
+
+$("input[type='checkbox'][class='security-req']").on("change",function(){
+  if($(this).is(':checked')){
+    $("#special-security").show();
+  }else{
+    $("#special-security").hide();
+  }
+});
+
+$("input[type='checkbox'][class='doctor-req']").on("change",function(){
+  if($(this).is(':checked')){
+    $("#special-doctor").show();
+  }else{
+    $("#special-doctor").hide();
+  }
+});
+
+
+
+$("input[type='checkbox']").on("change",function(){
+  if($(this).is(':checked')){
+    $("#no-filter").remove();
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
