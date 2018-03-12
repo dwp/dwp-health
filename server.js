0f987df4487a3b3877df49416f403c93229326f1
jamie crisp <jamescrispdigital@gmail.com>
2018-03-12 16:13:57 +0000

    created v6 folder

--- server.js
+++ server.js
@@ -229,6 +229,7 @@ if (typeof (routes) !== 'function') {
   app.use('/assess/v3-1/', require('./app/views/assess/v3-1/_routes.js'))
   app.use('/assess/v4/', require('./app/views/assess/v4/_routes.js')) 
   app.use('/assess/v5/', require('./app/views/assess/v5/_routes.js'))    
+  app.use('/assess/v6/', require('./app/views/assess/v6/_routes.js'))    
   app.use('/vouch/', require('./app/views/vouch/_routes.js'))
   app.use('/esa113/', require('./app/views/esa113/_routes.js'))
   app.use('/', routes)
