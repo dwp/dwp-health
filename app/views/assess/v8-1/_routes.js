8c62c75cd24633bd54cf169d699c4344f00e290f
jamie crisp <jamiecrisp84@gmail@gmail.com>
2018-04-26 10:47:00 +0100

    added re-book appointment to MVP prototype

--- app/views/assess/v8-1/_routes.js
+++ app/views/assess/v8-1/_routes.js
@@ -140,6 +140,36 @@ router.get('/capacity/manage-centre/:centreId/manage-staff/:section', function(r
 });
 
 
+router.get('/booking/timepicker', function(req, res, next){
+  slotsData = require('../../../../app/views/assess/v9/slots-data-booking.js')
+
+  getCentreDetails(req, res)
+  res.locals.staff = require('../../../../app/views/assess/v9/data/staff.js')
+  res.locals.staffTotals = {}
+  res.locals.query.day = req.query.day || "monday";
+  res.locals.slots = slotsData[res.locals.query.day];
+
+  res.locals.staffTotals.available = res.locals.staff.filter(function(obj){
+    if(obj.scrutinyPaperwork && obj.days[res.locals.query.day].scrutiny){
+      return false;
+    } else {
+      return obj.days[res.locals.query.day].available
+    }
+  }).length;
+
+  var totalAppointments = 0;
+
+  slotsData[res.locals.query.day].map(day => totalAppointments = totalAppointments + day.usedSlots);
+  res.locals.totalAppointments = totalAppointments;
+  res.locals.slots = slotsData[res.locals.query.day];
+  res.locals.totalSlots = slotsData[res.locals.query.day].length;
+
+  res.locals.totalAvailableAppointments = res.locals.staffTotals.available * res.locals.totalSlots;
+
+  next()
+})
+
+
 
 router.get('/capacity/manage-centre/capacity', function(req, res, next){
 
